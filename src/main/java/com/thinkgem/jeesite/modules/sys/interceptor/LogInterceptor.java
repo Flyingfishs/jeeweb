41fc36b17ed60f8ec63c7794b4b821d19474536c
andysofan <andy_wu2012@163.com>
2015-08-06 15:21:34 +0800

    init

---
+++ src/main/java/com/thinkgem/jeesite/modules/sys/interceptor/LogInterceptor.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright &copy; 2012-2014 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package com.thinkgem.jeesite.modules.sys.interceptor;
+
+import java.text.SimpleDateFormat;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.NamedThreadLocal;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.thinkgem.jeesite.common.service.BaseService;
+import com.thinkgem.jeesite.common.utils.DateUtils;
+import com.thinkgem.jeesite.modules.sys.utils.LogUtils;
+
+/**
+ * 日志拦截器
+ * @author ThinkGem
+ * @version 2014-8-19
+ */
+public class LogInterceptor extends BaseService implements HandlerInterceptor {
+
+	private static final ThreadLocal<Long> startTimeThreadLocal =
+			new NamedThreadLocal<Long>("ThreadLocal StartTime");
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, 
+			Object handler) throws Exception {
+		if (logger.isDebugEnabled()){
+			long beginTime = System.currentTimeMillis();//1、开始时间  
+	        startTimeThreadLocal.set(beginTime);		//线程绑定变量（该数据只有当前请求的线程可见）  
+	        logger.debug("开始计时: {}  URI: {}", new SimpleDateFormat("hh:mm:ss.SSS")
+	        	.format(beginTime), request.getRequestURI());
+		}
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, 
+			ModelAndView modelAndView) throws Exception {
+		if (modelAndView != null){
+			logger.info("ViewName: " + modelAndView.getViewName());
+		}
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, 
+			Object handler, Exception ex) throws Exception {
+
+		// 保存日志
+		String url = request.getRequestURI();//过滤日志记录
+		//if(url.endsWith("/")||url.endsWith("/a")||url.endsWith("info")||url.endsWith("index")||url.endsWith("list")||url.endsWith("treeData")||url.endsWith("none")||url.endsWith("tree")){
+		//}else 
+			LogUtils.saveLog(request, handler, ex, null);
+		
+		// 打印JVM信息。
+		if (logger.isDebugEnabled()){
+			long beginTime = startTimeThreadLocal.get();//得到线程绑定的局部变量（开始时间）  
+			long endTime = System.currentTimeMillis(); 	//2、结束时间  
+	        logger.debug("计时结束：{}  耗时：{}  URI: {}  最大内存: {}m  已分配内存: {}m  已分配内存中的剩余空间: {}m  最大可用内存: {}m",
+	        		new SimpleDateFormat("hh:mm:ss.SSS").format(endTime), DateUtils.formatDateTime(endTime - beginTime),
+					request.getRequestURI(), Runtime.getRuntime().maxMemory()/1024/1024, Runtime.getRuntime().totalMemory()/1024/1024, Runtime.getRuntime().freeMemory()/1024/1024, 
+					(Runtime.getRuntime().maxMemory()-Runtime.getRuntime().totalMemory()+Runtime.getRuntime().freeMemory())/1024/1024); 
+		}
+		
+	}
+
+}
